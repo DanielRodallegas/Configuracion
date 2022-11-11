03c37aa962467c9532dba9d21b1825cbc159be56
DanielRodallegas <116135287+DanielRodallegas@users.noreply.github.com>
2022-11-11 08:36:14 -0600

    Add files via upload

---
+++ configuracion_perfil/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace configuracion_perfil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuario;
+            string contraseña;
+            int colores; 
+            
+
+            
+
+            Console.WriteLine("Bienvenido a la configuracion de su perfil \n");
+
+            Console.WriteLine("Ingrese su usuaio");
+            usuario = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su contraseña");
+            contraseña = Console.ReadLine();
+
+
+            Console.WriteLine("Eliga el color de consola que quiera");
+            colores = Convert.ToByte(Console.ReadLine());
+
+            if (colores==1)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+            }
+
+            if (colores==2)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+            }
+
+            if (colores == 3)
+            {
+                Console.BackgroundColor = ConsoleColor.Magenta;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
